327749dcfaad5ad4c94926029f076251497064ca
Murtadha Alhgam <myEmail.com>
2022-09-02 12:30:31 +0200

    Eq:1 To output a Sentence on the Console

---
+++ Unit2_C_PROGRAMMING/C_BasicsAssignments/Homework_1/EX1_Print a Sentence.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Print a Sentence.c
+ Author      : Murtadha Alhgam
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("C Programming"); /* prints>>  C Programming */
+	return EXIT_SUCCESS;
+}
