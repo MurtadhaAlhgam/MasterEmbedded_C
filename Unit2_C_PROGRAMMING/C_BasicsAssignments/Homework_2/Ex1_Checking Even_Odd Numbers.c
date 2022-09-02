e3d5b6f4904038853a47aa3c181ad2fbdbeac721
Murtadha Alhgam <myEmail.com>
2022-09-02 15:58:38 +0200

    Eq1_Checking Even_Odd numbers

---
+++ Unit2_C_PROGRAMMING/C_BasicsAssignments/Homework_2/Ex1_Checking Even_Odd Numbers.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : Checking.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int num;
+	printf("Enter an integer you want to check:");
+	fflush(stdin);  fflush(stdout);
+	scanf("%d", &num);
+	if((num %2)==0)
+	printf("(%d) is even",num);
+	else
+	printf("(%d) is odd",num);
+	return EXIT_SUCCESS;
+}
